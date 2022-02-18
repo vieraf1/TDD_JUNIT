8d68ff2d2455983e7b8bd1c0302904a007dcf3bd
Lucas Gabriel Coelho Vieira <vieraf1@hotmail.com.br>
2022-02-17 22:16:07 -0300

    Adaptando projeto ao Maven
    Adaptando projeto ao Maven

---
+++ src/main/java/tdd/service/BonusService.java
@@ -0,0 +1,17 @@
+package tdd.service;
+
+import java.math.BigDecimal;
+
+import tdd.modelo.Funcionario;
+
+public class BonusService {
+
+	public BigDecimal calcularBonus(Funcionario funcionario) {
+		BigDecimal valor = funcionario.getSalario().multiply(new BigDecimal("0.1"));
+		if (valor.compareTo(new BigDecimal("1000")) > 0) {
+			valor = BigDecimal.ZERO;
+		}
+		return valor;
+	}
+
+}
--- src/tdd/Calculadora.java
+++
@@ -1,9 +0,0 @@
-package tdd;
-
-public class Calculadora {
-
-	public int somar(int a, int b) {
-		return a + b;
-	}
-	
-}
